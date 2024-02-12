2c2f04e3399c1cf6c636bcc8c5d6f98948314e67
DESKTOP-4QIAOD6\Я <galiyaismaeva@yandex.ru>
2024-02-12 18:00:16 +0300

    second commit

---
+++ Homework4/exercise1/exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        int start = 201;
+        int end = 500;
+        string result = "200";
+
+        while (start <= end)
+        {
+            if (start % 17 == 0) 
+            {
+                result += ',' + start.ToString();
+            }
+            
+            start++;
+        }
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
---
+++ Homework4/exercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("******" + "\n" + "\n" + "   AAA   " + "\n"  + "\n" + "******");
+
+        for (;;)
+        {
+            Console.WriteLine("\n" + "Введите один из символов: W, S, A, D.");
+
+            string action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "W":
+                    Console.WriteLine("Сдвиг вверх");
+                    Console.WriteLine("******" + "\n" + "   AAA   " + "\n" + "\n" + "******");
+                    continue;
+                case "S":
+                    Console.WriteLine("Сдвиг вниз");
+                    Console.WriteLine("******" + "\n" + "\n" + "   AAA   "  + "\n" + "******");
+                    continue;
+                case "A":
+                    Console.WriteLine("Сдвиг вправо");
+                    Console.WriteLine("******" + "\n" + "\n" + "      AAA" + "\n" + "\n" + "******");
+                    continue;
+                case "D":
+                    Console.WriteLine("Сдвиг влево");
+                    Console.WriteLine("******" + "\n" + "\n" + "AAA   " + "\n" + "\n" + "******");
+                    continue;
+                default:
+                    Console.WriteLine("\n" + "Введен символ \"{0}\" вне перечня - перемещение не выполнено", action);
+                    break;
+            }
+
+            break;
+
+        }
+
+        Console.WriteLine("\n" + "Конец игры");
+
+        Console.ReadLine();
+
+    }
+}
