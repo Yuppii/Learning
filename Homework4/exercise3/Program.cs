3a522c07377ffe701dd06e06c93a62cab7d60e45
DESKTOP-4QIAOD6\Я <galiyaismaeva@yandex.ru>
2024-02-12 18:10:24 +0300

    second commit

--- Homework4/exercise3/Program.cs
+++ Homework4/exercise3/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using System.Reflection.Emit;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = new int[10];
+        Random rnd = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next();
+        }
+
+        int counter = array.Length;
+
+    Label:
+        counter--;
+        Console.WriteLine(array[counter]);
+
+        if(counter > 0)
+        {
+            goto Label;
+        }
+
+        Console.ReadLine();
     }
 } 
---
+++ Homework4/exercise4/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите Ваш доход за месяц в рублях: ");
+        string? input = Console.ReadLine();
+
+        if (decimal.TryParse(input, out decimal taxByIncome) == true)
+        {
+
+            if (CountTax(ref taxByIncome) == true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ваш налог на доход составил {0} рублей", taxByIncome);
+            }
+            else 
+            { 
+                Console.WriteLine("Налог не начислен"); 
+            }
+
+            
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное значение");
+        }
+
+        Console.ReadLine();
+
+    }
+
+    public static bool CountTax (ref decimal taxByIncome)
+    {
+
+        if (taxByIncome < 0)
+        {
+            return false;
+        }
+        else
+        {
+            const decimal tax = (decimal)0.13;
+
+            taxByIncome = decimal.Multiply(taxByIncome, tax);
+
+            return true;
+        }
+
+    }
+}
