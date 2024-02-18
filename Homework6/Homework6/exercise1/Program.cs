4acdccb58b0e81f18220d3b976ed58e97b46c673
DESKTOP-4QIAOD6\Я <galiyaismaeva@yandex.ru>
2024-02-18 22:15:12 +0300

    homework 6 arrays

---
+++ Homework6/Homework6/exercise1/Program.cs
@@ -0,0 +1,70 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        string[] array = { "apple", "banana", "orange" };
+        string? value = "banana";
+
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("Пустой массив");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(value) == true)
+        {
+
+            return;
+
+        }
+
+        array = AddValueToArray(array, value);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+
+        }
+
+        Console.ReadLine();
+
+    }
+
+    public static string[] AddValueToArray(string[] array, string? value)
+    {
+
+        if (string.IsNullOrEmpty(value) == true || array == null)
+        {
+
+            return null;
+
+        }
+
+        string valueToUpper = value.ToUpper();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].ToUpper() == valueToUpper)
+            {
+                return array;
+            }
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == null)
+            {
+                array[i] = value;
+                return array;
+            }
+        }
+
+        Array.Resize(ref array, array.Length + 1);
+        int ln = array.Length - 1;
+        array[ln] = value;
+
+        return array;
+
+    }
+
+}
---
+++ Homework6/Homework6/exercise2/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    static int aLenght;
+    static int aHeight;
+
+
+    static int bLenght;
+    static int bHeight;
+
+    static int maxLength;
+    static int maxHeight;
+
+    private static void Main()
+    {
+        //int[,] a = { { 1, 2, 3 }, { 1, 2, 3 } };
+        int[,] a = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+        int[,] b = { { 1, 2, 3 }, { 1, 2, 3 } };
+
+        aLenght = a.GetLength(0);
+        aHeight = a.GetLength(1);
+
+        bLenght = b.GetLength(0);
+        bHeight = b.GetLength(1);
+
+        maxLength = aLenght > bLenght ? aLenght : bLenght;
+        maxHeight = aHeight > bHeight ? aHeight : bHeight;
+
+        aLenght--;
+        aHeight--;
+
+        bLenght--;
+        bHeight--;
+
+        int[,] result = SumArray(a, b);
+
+        Console.WriteLine("Массив A + массив B = "); 
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            for (int j = 0; j < maxHeight; j++)
+            {
+                Console.Write(result[i, j]);
+                Console.Write(", ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+        Console.ReadLine();
+    }
+
+    private static int[,] SumArray(int[,] a, int[,] b)
+    {
+        int[,] result = new int[maxLength, maxHeight];
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            for (int j= 0; j < maxHeight; j++)
+            {
+                result[i, j] = (aLenght < i || aHeight < j ? 0 : a[i, j]) + (bLenght < i || bHeight < j ? 0 : b[i, j]);
+            }
+        }
+
+        return result;
+
+    }
+}
